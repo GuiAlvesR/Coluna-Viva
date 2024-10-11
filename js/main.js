cc3d0af2672288e6df5fa4693937d009ec0f19f3
GuiAlvesR <guinegocios20@gmail.com>
2024-10-11 13:32:16 -0300

    Update faq in index and style in css and alter imagens

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+function p(num, src) {
+  const p = document.getElementById(`p${num}`);
+  const number = src.querySelector(".faq-number");
+  const img = src.querySelector("img");
+
+  if (p.classList.contains("faq-p")) {
+    p.classList.remove("faq-p");
+    number.classList.remove("faq-number-opacity");
+    img.classList.remove("seta");
+  } else {
+    p.classList.add("faq-p");
+    number.classList.add("faq-number-opacity");
+    img.classList.add("seta");
+  }
+}
