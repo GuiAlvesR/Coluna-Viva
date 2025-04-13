4cff60b014ca3afe65b2e50037174c7b5453bba2
GuiAlvesR <guinegocios20@gmail.com>
2025-04-12 23:32:10 -0300

    Later version actualizity

--- js/main.js
+++ js/main.js
@@ -36,16 +36,18 @@ let totalTime = 40 * 60;
 function formatTime(seconds) {
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
-  return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+  return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
 }
 
-let timeLeft = localStorage.getItem('timeLeft') ? parseInt(localStorage.getItem('timeLeft')) : totalTime;
+let timeLeft = localStorage.getItem("timeLeft")
+  ? parseInt(localStorage.getItem("timeLeft"))
+  : totalTime;
 
 function updateTimer() {
-  const timerDisplay = document.getElementById('timer');
+  const timerDisplay = document.getElementById("timer");
   timerDisplay.textContent = formatTime(timeLeft);
 
-  localStorage.setItem('timeLeft', timeLeft);
+  localStorage.setItem("timeLeft", timeLeft);
 
   if (timeLeft > 0) {
     timeLeft--;
@@ -57,19 +59,61 @@ const timerInterval = setInterval(updateTimer, 1000);
 
 updateTimer();
 
-const timerBar = document.getElementById('timer-bar');
-const faqsSection = document.querySelector('.section-faqs');
+const timerBar = document.getElementById("timer-bar");
+const faqsSection = document.querySelector(".section-faqs");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      timerBar.classList.add('visible');
-    } else {
-      timerBar.classList.remove('visible');
-    }
-  });
-}, {
-  threshold: 0.1
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        timerBar.classList.add("visible");
+      } else {
+        timerBar.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
 
 observer.observe(faqsSection);
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      direction: "vertical",
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      direction: "horizontal",
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      direction: "horizontal",
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
